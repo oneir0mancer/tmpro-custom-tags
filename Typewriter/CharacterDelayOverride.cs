f6ef4dd7da6dc725212b92e31cc21969d90d4f3a
Andrey Sugonyaev <34348146+oneir0mancer@users.noreply.github.com>
2023-06-03 18:10:27 +0300

    Add typewriter

---
+++ Typewriter/CharacterDelayOverride.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Oneiromancer.TMP.Typewriter
+{
+    [System.Serializable]
+    public class CharacterDelayOverride
+    {
+        public float TimeDelay => _timeDelay;
+
+        [SerializeField] private string _characters;
+        [SerializeField, Min(0)] private float _timeDelay;
+
+        public bool Contains(char character) => _characters.Contains(character);
+    }
+}
---
+++ Typewriter/TypewriterAnimation.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace Oneiromancer.TMP.Typewriter
+{
+    public class TypewriterAnimation : MonoBehaviour
+    {
+        public event System.Action TickEvent;
+        public event System.Action<char> CharacterRenderedEvent;
+        
+        [SerializeField] private TMP_Text _text;
+        [SerializeField, Min(0)] private float _delayBetweenCharacters = 0.1f;
+        [Tooltip("Override time delay for a set of specific characters")]
+        [SerializeField] private CharacterDelayOverride[] _delayOverrides;
+        [SerializeField] private bool _playOnStart;
+
+        private Coroutine _coroutine;
+        private TMP_MeshInfo[] _cache;
+
+        // Subscribe to TMP event of redrawing mesh
+        private void OnEnable() => TMPro_EventManager.TEXT_CHANGED_EVENT.Add(OnTMProChanged);
+        private void OnDisable() => TMPro_EventManager.TEXT_CHANGED_EVENT.Remove(OnTMProChanged);
+
+        private void Start()
+        {
+            _text.maxVisibleCharacters = 0;
+            if (_playOnStart) Play();
+        }
+
+        /// Play typewriter animation, starting with the first character
+        public void Play()
+        {
+            _coroutine ??= StartCoroutine(TypewriterCoroutine());
+        }
+
+        /// Pause typewriter animation
+        public void Pause()
+        {
+            if (_coroutine != null) StopCoroutine(_coroutine);
+            _coroutine = null;
+        }
+        
+        /// Stop and reset typewriter animation
+        public void Stop()
+        {
+            Pause();
+            _text.maxVisibleCharacters = 0;
+        }
+        
+        /// Resume typewriter animation from the last rendered character
+        public void Resume()
+        {
+            _coroutine ??= StartCoroutine(TypewriterCoroutine(_text.maxVisibleCharacters));
+        }
+
+        private IEnumerator TypewriterCoroutine(int startIdx = 0)
+        {
+            _text.ForceMeshUpdate();
+            var count = _text.textInfo.characterCount;
+
+            yield return new WaitForSeconds(_delayBetweenCharacters);
+            
+            for (int i = startIdx; i < count; i++)
+            {
+                Tick(i);
+                
+                var currentChar = _text.textInfo.characterInfo[i].character;
+                CharacterRenderedEvent?.Invoke(currentChar);
+                
+                var time = _delayBetweenCharacters;
+                foreach (var characterOverride in _delayOverrides)
+                {
+                    if (characterOverride.Contains(currentChar)) time = characterOverride.TimeDelay;
+                    break;
+                }
+                
+                yield return new WaitForSeconds(time);
+            }
+        }
+
+        private void Tick(int index)
+        {
+            _cache = _text.textInfo.CopyMeshInfoVertexData();    //Cache vertex information before redrawing mesh
+            
+            _text.maxVisibleCharacters = index + 1;
+            TickEvent?.Invoke();
+        }
+
+        private void RestoreCache()
+        {
+            if (_cache == null) return;
+            
+            for (int i = 0; i < _text.textInfo.meshInfo.Length; i++)
+            {
+                _text.textInfo.meshInfo[i].mesh.vertices = _cache[i].vertices;    //Restore vertices positions
+                _text.textInfo.meshInfo[i].mesh.colors32 = _cache[i].colors32;    //Restore vertices colors
+                _text.UpdateGeometry(_text.textInfo.meshInfo[i].mesh, i);
+            }
+        }
+        
+        private void OnTMProChanged(Object obj)
+        {
+            if (obj == _text) RestoreCache();
+        }
+        
+        private void OnValidate()
+        {
+            _text ??= GetComponent<TMP_Text>();
+        }
+    }
+}
